a5b197dbe51404df6f39a9ff03740d18dbee7a61
Ian Shipman <ics@gambolingpangolin.com>
2022-02-19 19:46:43 -0600

    Adds nix shell definition; updates lockfile

---
+++ shell.nix
@@ -0,0 +1,24 @@
+# Use nix to develop the site without putting any Ruby stuff into the default
+# environment
+
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    ruby
+    bundler
+  ];
+
+  shellHook = ''
+    echo Installing ...
+    bundle install &> /dev/null
+
+    function cbd_build { bundle exec jekyll build; }
+    function cbd_serve { bundle exec jekyll serve --watch; }
+
+    echo cbd_build -- build the project
+    echo cbd_serve -- start a webserver serving the site which automaticaly rebuilds the project
+  '';
+}
